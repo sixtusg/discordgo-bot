59ab4409c9500759509cdac9808a340838bbb5f2
sixtusg <sixtus.gomez@gmail.com>
2021-07-24 21:28:45 -0500

    📦NEW: revamped help command; spaghettified all code in the process :(

--- botFunctions.go
+++ botFunctions.go
@@ -9,7 +9,14 @@ import(
 
 //simple
 func help(s *discordgo.Session, m *discordgo.MessageCreate) {
-    s.ChannelMessageSend(m.ChannelID, "Commands: `" + BotPrefix + "help, " + BotPrefix + "ping, " + BotPrefix + "ban" + "`") //note to self: this is horrendously ugly and must be fixed, especially when considering that it is likely the first command users will issue. in the future, add embeds and order in a hierarchal manner
+  //if this isn't the most tedious thing i've ever done...
+  //is there any way to not make this code ugly?
+  //yours truly, past Sixtus
+  banHelp := "**Ban**\n`" + BotPrefix + "ban help`\n\n"
+  unbanHelp := "**Unban**\n`" + BotPrefix + "unban help`\n\n"
+  kickHelp := "**Kick**\n`" + BotPrefix + "kick help`\n\n"
+
+  genericEmbed("Commands", banHelp + unbanHelp + kickHelp, s, m)
 }
 
 func ping(s *discordgo.Session, m *discordgo.MessageCreate) {
@@ -18,7 +25,7 @@ func ping(s *discordgo.Session, m *discordgo.MessageCreate) {
 }
 
 //moderation
-func ban(s *discordgo.Session, m *discordgo.MessageCreate) {
+func ban(s *discordgo.Session, m *discordgo.MessageCreate) { //spaghetti function
   p, err := s.UserChannelPermissions(m.Author.ID, m.ChannelID)
 
   if err != nil {
@@ -28,13 +35,24 @@ func ban(s *discordgo.Session, m *discordgo.MessageCreate) {
   if p&discordgo.PermissionBanMembers == discordgo.PermissionBanMembers {
     arg := strings.Fields(m.Content)
 
-    if len(arg) < 3 {
+    if len(arg) < 2 {
       errEmbed("Syntax error", BotPrefix + "ban [mention user] [number of days]", s, m)
       return
     }
 
     if arg[1] == "@everyone" {
       errEmbed("Syntax error", "You cannot issue this command with argument @everyone", s, m)
+      return
+    }
+
+    if arg[1] == "help" {
+      genericEmbed("Help", "Use this command to ban a user\n\n**Usage**\n`" + BotPrefix + "ban [mention user] [number of days for user to be banned]`", s, m)
+      return
+    }
+
+    if len(arg) < 3 {
+      errEmbed("Syntax error", BotPrefix + "ban [mention user] [number of days]", s, m)
+      return
     }
 
     if strings.HasPrefix(arg[1], "<@!") {
@@ -72,6 +90,12 @@ func unban(s *discordgo.Session, m *discordgo.MessageCreate) {
 
     if arg[1] == "@everyone" {
       errEmbed("Syntax error", "You cannot issue this command with argument @everyone", s, m)
+      return
+    }
+
+    if arg[1] == "help" {
+      genericEmbed("Help", "Use this command to unban a user\n\n**Usage**\n`" + BotPrefix + "unban <@![user id]`", s, m)
+      return
     }
 
     if strings.HasPrefix(arg[1], "<@!") {
@@ -105,10 +129,16 @@ func kick(s *discordgo.Session, m *discordgo.MessageCreate) {
       errEmbed("Syntax error", "You cannot issue this command with argument @everyone", s, m)
     }
 
+    if arg[1] == "help" {
+      genericEmbed("Help", "Use this command to kick a user\n\n**Usage**\n`" + BotPrefix + "kick [mention a user]`", s, m)
+    }
+
     if strings.HasPrefix(arg[1], "<@!") {
       id := getIDFromMention(arg[1])
 
       s.GuildMemberDelete(m.GuildID, id)
+    } else {
+      errEmbed("Syntax error", BotPrefix + "unban [mention user]", s, m)
     }
   } else {
     errEmbed("Error", "You do not have permission to issue this command.", s, m)
